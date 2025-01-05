c9396c3051d44c6d24487f3d7eb3a1236c7cb9f2
itsbk13 <barathsrini13@gmail.com>
2025-01-05 19:26:08 +0530

    Added dummy Flask web service

--- dummy.py
+++ dummy.py
@@ -1,14 +1,12 @@
-import os
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-# Use the environment variable or default to 8080
-port = os.getenv("PORT", 8080)
-
 @app.route('/')
 def home():
-    return "Hello World!"
+    return "Hello, this is your bot's web service!"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 10000)
     app.run(host='0.0.0.0', port=port)
